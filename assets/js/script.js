43696f18a8b06d9982288682a49034c3cfa0f4e5
KAYSILVHA <kaysilvha@gmail.com>
2024-06-19 18:57:25 -0300

    novas funcionalidades

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function showImageKitana() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/kitana-avatar.png";
+}
+
+function showImageKratos() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/kratos-avatar.png";
+}
+
+function showImageKungLao() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/kungLao-avatar.png";
+}
+
+
+function showImageSubZero() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/subZero-avatar.png";
+}
+
+
+function showImageScorpion() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/scorpion-avatar.png";
+}
+
+
+function showImageSindel() {
+    const image = document.getElementById('characterImage');
+    image.src = "../assets/images/sindel-avatar.png";
+}
