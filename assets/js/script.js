8997d43982609f50873bb72883aaa200c1a70e05
KAYSILVHA <kaysilvha@gmail.com>
2024-06-24 19:09:43 -0300

    ajustes finais

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 const imageInitial = document.querySelector(".image-initial");
+const characterName = document.getElementById("character-name");
 
 function showImageKitana() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/kitana-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Kitana"
 }
 
 function showImageKratos() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/kratos-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Kratos"
 }
 
 function showImageKungLao() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/kungLao-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Kung Lao"
 }
 
 
@@ -23,6 +27,7 @@ function showImageSubZero() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/subZero-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Sub Zero"
 }
 
 
@@ -30,6 +35,7 @@ function showImageScorpion() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/scorpion-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Scorpion"
 }
 
 
@@ -37,6 +43,7 @@ function showImageSindel() {
     const image = document.getElementById('characterImage');
     image.src = "../assets/images/sindel-avatar.png";
     imageInitial.setAttribute("class", "");
+    characterName.innerText = "Sindel"
 }
 
 
